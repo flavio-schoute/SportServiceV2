f1efc1b66be37baf4b44e5eafe192295092323b6
Flavio Schoute <flavio03@outlook.com>
2021-06-07 11:15:45 +0200

    Feat: Added activity overview loading the data from the database

--- app/Http/Controllers/SportService/ActivityOverviewController.php
+++ app/Http/Controllers/SportService/ActivityOverviewController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\SportService;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sports;
 use Illuminate\Http\Request;
 
 class ActivityOverviewController extends Controller {
 
     public function index() {
-        return view('school.activity-overview');
+        $activity = Sports::select('name', 'description')->get();
+
+        return view('school.activity-overview', [
+            'activities' => $activity
+        ]);
     }
 }
