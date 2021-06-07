b7601209b72388831cb68aee30dae74a8b25e214
Flavio Schoute <flavio03@outlook.com>
2021-06-07 11:29:30 +0200

    Refactor: Removed old code from activity overview

--- resources/views/school/activity-overview.blade.php
+++ resources/views/school/activity-overview.blade.php
@@ -19,17 +19,7 @@
                     </div>
                 @endforeach
             </div>
-
-
-{{--            @foreach($activities as $activity)--}}
-{{--                <div>--}}
-{{--                    <div class="flex flex-col max-w-md bg-white px-8 py-6 rounded-xl space-y-5 items-center">--}}
-{{--                        <h3 class="font-serif font-bold text-gray-900 text-xl">{{$activity->name}}</h3>--}}
-{{--                        <p class="text-center leading-relaxed">{{$activity->description}}</p>--}}
-{{--                        <button class="px-24 py-4 bg-gray-900 rounded-md text-white text-sm focus:border-transparent">Aanmelden</button>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            @endforeach--}}
+            
 
             <footer class="pt-3 mt-4 text-gray-500 border-t-2 border-gray-400">
                 © {{ now()->year }}
