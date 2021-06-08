a97c421f7d85d7b362a5698cebd6340f938abfcc
Flavio Schoute <flavio03@outlook.com>
2021-06-08 10:22:33 +0200

    Hot: Fixed typed confusion

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -21,7 +21,7 @@
                 @if(auth()->user()->is_admin == 1)
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                         <x-jet-nav-link>
-                            {{ __('Accounts aanmaken') }}
+                            {{ __('Scholen aanmaken') }}
                         </x-jet-nav-link>
                     </div>
 
--- resources/views/school/add-students.blade.php
+++ resources/views/school/add-students.blade.php
@@ -8,13 +8,80 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            <div class="grid grid-cols-3 gap-4">
-                    <div>
-                        <div class="flex flex-col max-w-md bg-white px-8 py-6 rounded-xl space-y-5 items-center">
-                            <button class="px-24 py-4 bg-blue-900 rounded-md text-white text-sm focus:border-transparent">Aanmelden</button>
-                        </div>
+            <!-- School -->
+            <!-- First name -->
+            <!-- Last name -->
+            <!-- Group -->
+            <!-- Date of birth -->
+
+            <div class="grid bg-white rounded-lg shadow-xl w-11/12 md:w-9/12 lg:w-1/2">
+                <div class="flex justify-center py-4">
+                    <div class="flex bg-purple-200 rounded-full md:p-4 p-2 border-2 border-purple-300">
+                        <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
+                        </svg>
                     </div>
+                </div>
+
+                <div class="flex justify-center">
+                    <div class="flex">
+                        <h1 class="text-gray-600 font-bold md:text-2xl text-xl">Tailwind Form</h1>
+                    </div>
+                </div>
+
+                <div class="grid grid-cols-1 mt-5 mx-7">
+                    <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 1</label>
+                    <input
+                        class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                        type="text" placeholder="Input 1"/>
+                </div>
+
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
+                    <div class="grid grid-cols-1">
+                        <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Voornaam</label>
+                        <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                            type="text" placeholder="Pieter"/>
+                    </div>
+
+                    <div class="grid grid-cols-1">
+                        <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Achternaam</label>
+                        <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                            type="text" placeholder="van Wols"/>
+                    </div>
+                </div>
+
+                <div class="grid grid-cols-1 mt-5 mx-7">
+                    <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Selection</label>
+                    <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                        @for($i = 4; $i < 9; $i++)
+                            <option value="{{ $i }}">Groep {{$i}}</option>
+                        @endfor
+                    </select>
+                </div>
+
+                <div class="grid grid-cols-1 mt-5 mx-7">
+                    <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 1</label>
+                    <input
+                        class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                        type="date" placeholder=""/>
+                </div>
+
+
+                <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
+                    <button
+                        class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>
+                        Cancel
+                    </button>
+                    <button
+                        class='w-auto bg-purple-500 hover:bg-purple-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>
+                        Create
+                    </button>
+                </div>
+
             </div>
+
 
             <footer class="pt-3 mt-4 text-gray-500 border-t-2 border-gray-400">
                 © {{ now()->year }}
