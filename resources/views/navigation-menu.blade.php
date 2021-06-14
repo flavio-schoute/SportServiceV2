2986ac972910edff2040b8e1f2dfc830229f89a8
RobinPater <400018894@st.roc.a12.nl>
2021-06-14 10:16:48 +0200

    feature: nav dropdown

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -19,29 +19,53 @@
 
                 <!-- SportService -->
                 @if(auth()->user()->is_admin == 1)
-                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <x-jet-nav-link href="{{ route('add-schools') }}" :active="request()->routeIs('add-schools')">
-                            {{ __('Scholen aanmaken') }}
-                        </x-jet-nav-link>
-                    </div>
+				
+					 <div class="hidden sm:flex sm:items-center sm:ml-6">
+						<x-jet-dropdown align="right" width="48">
+							<x-slot name="trigger">
+
+								<button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+									<div>Invoer opties</div>
+
+									<div class="ml-1">
+										<svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+											<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+										</svg>
+									</div>
+								</button>
+
+							</x-slot>
+
+							<x-slot name="content">
+
+								<!-- User Management -->
+								<div class="block px-4 py-2 text-xs text-gray-400">
+									{{ __('Opties') }}
+								</div>
+
+								<x-jet-dropdown-link href="{{ route('add-providers') }}" :active="request()->routeIs('add-providers')">
+									{{ __('Aanbieders invoeren') }}
+								</x-jet-dropdown-link>
+								
+								<x-jet-dropdown-link href="{{ route('add-schools') }}" :active="request()->routeIs('add-schools')">
+									{{ __('Scholen aanmaken') }}
+								</x-jet-dropdown-link>
+								
+								<x-jet-dropdown-link href="{{ route('add-teachers') }}" :active="request()->routeIs('add-teachers')">
+									{{ __('Leraren invoeren') }}
+								</x-jet-dropdown-link>
+
+							</x-slot>
+
+						</x-jet-dropdown>
+					</div>
+			
 
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                         <x-jet-nav-link>
                             {{ __('Activiteiten invoeren') }}
                         </x-jet-nav-link>
                     </div>
-
-					<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-						<x-jet-nav-link href="{{ route('add-teachers') }}" :active="request()->routeIs('add-teachers')">
-							{{ __('leraren invoeren') }}
-						</x-jet-nav-link>
-					</div>
-
-					<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-						<x-jet-nav-link href="{{ route('add-providers') }}" :active="request()->routeIs('add-providers')">
-							{{ __('Aanbieders invoeren') }}
-						</x-jet-nav-link>
-					</div>
 
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                         <x-jet-nav-link>
