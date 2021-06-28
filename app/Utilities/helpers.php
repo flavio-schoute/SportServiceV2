3f05aa4dce7aa8bd67ab00fc3ba01a7eea1eac27
Flavio Schoute <flavio03@outlook.com>
2021-06-28 18:29:47 +0200

    Added helper function into the helper classes
    Need to implment and docuemnt

--- app/Utilities/helpers.php
+++ app/Utilities/helpers.php
@@ -1,5 +1,26 @@
 <?php
 
-if (!function_exists('xxxxxtodo')) {
-//    Create function todo
+use Illuminate\Support\Facades\DB;
+
+if (!function_exists('isOptionDisabled')) {
+
+    function isOptionDisabled($key) {
+        $data = getOptions($key);
+        foreach($data as $item) {
+            if ($item->value == 1) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
+if (!function_exists('getOptions')) {
+
+    // Document todo -> collection return??
+    function getOptions($key)
+    {
+        return DB::table('options')->select('key', 'value')->where('key', '=', $key)->get();
+    }
 }
