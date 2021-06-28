e02d477f672ff7b07949ef84bc2ff64a17b837de
Flavio Schoute <flavio03@outlook.com>
2021-06-28 17:21:21 +0200

    Added model

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Teacher extends Model
 {
@@ -14,27 +15,53 @@ class Teacher extends Model
      *
      * @var string
      */
-    protected $primaryKey = 'teacher_id';
+    protected $primaryKey = 'student_id';
+
 
     /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
      */
-	public $timestamps = false;
+    public $timestamps = false;
 
-	 /**
+    /**
      * The table associated with the model.
      *
      * @var string
      */
-    protected $table = 'teacher';
+    protected $table = 'students';
 
-	protected $fillable = [
-		'id_school',
+    protected $fillable = [
+        'id_school',
         'first_name',
-		'last_name',
-		'email',
-		'phone_number'
+        'last_name',
+        'group',
+        'date_of_birth',
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'student_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id_school' => 'integer',
+        'group' => 'integer',
+        'date_of_birth' => 'date',
+    ];
+
+    public function school(): BelongsTo
+    {
+        return $this->belongsTo(School::class, 'id_school', 'school_id');
+    }
 }
