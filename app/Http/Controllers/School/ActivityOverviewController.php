76b25c6f36288d33a5c846614951d325d9f112f8
GekkeGlenn-Dev <glenn.dev@hotmail.com>
2021-07-07 00:47:06 +0200

    Changed the db query to Eloquent query using ActivityRegistration Model with relations in it.

--- app/Http/Controllers/School/ActivityOverviewController.php
+++ app/Http/Controllers/School/ActivityOverviewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\School;
 
 use App\Http\Controllers\Controller;
+use App\Models\ActivityRegistration;
 use Illuminate\Support\Facades\DB;
 
 class ActivityOverviewController extends Controller {
@@ -12,12 +13,20 @@ class ActivityOverviewController extends Controller {
 
         // Select the sport name and description from the activity_registration
         // And where id_school of the activity_registration equals the id_school in the users table
-        $activities = DB::table('activity_registration')
-            ->select('sports.name', 'sports.description')
-            ->leftJoin('activity_offer', 'activity_registration.uni_activity_name', '=', 'activity_offer.uni_activity_name')
-            ->leftJoin('sports', 'activity_offer.id_sport', '=', 'sports.sport_id')
-            ->where('activity_registration.id_school', '=', auth()->user()->id_school)
-            ->get();
+//        $activities = DB::table('activity_registration')
+//            ->select('sports.name', 'sports.description')
+//            ->leftJoin('activity_offer', 'activity_registration.uni_activity_name', '=', 'activity_offer.uni_activity_name')
+//            ->leftJoin('sports', 'activity_offer.id_sport', '=', 'sports.sport_id')
+//            ->where('activity_registration.id_school', '=', auth()->user()->id_school)
+//            ->get();
+
+        // improved.
+
+        // this get all the activityOffers with the right relation.
+        // its better to use ->paginate(25); instead of ->get();
+        // than you have automatic a pagination functionality in your webpage.
+        // use in the blade this to get the links: {{ $activities->links() }}
+        $activities = ActivityRegistration::with('activityOffer')->get();
 
 //        $test = School::with('sports')->get();
 //        dd($test);
